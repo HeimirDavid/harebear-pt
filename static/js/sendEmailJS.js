41ac05f374762d3577afd411d27a57d8d20fd2d0
HeimirDavid <heimir.97@gmail.com>
2020-08-24 15:26:24 +0200

    added rest of project

---
+++ static/js/sendEmailJS.js
@@ -0,0 +1,23 @@
+var successAlert = document.getElementById("success_alert");
+var failedAlert = document.getElementById("failed_alert");
+
+function sendMail(contactForm) {
+  emailjs
+    .send("gmail", "harebearpt", {
+      from_name: contactForm.full_name.value,
+      from_email: contactForm.email_address.value,
+      text_field: contactForm.message.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+        contactForm.reset();
+        successAlert.classList.add("show");
+      },
+      function (error) {
+        console.log("FAILED", error);
+        failedAlert.classList.add("show");
+      }
+    );
+  return false;
+}
