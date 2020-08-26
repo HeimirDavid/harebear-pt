a184403fa857733969bc704e9c8ae09c4a3fede9
HeimirDavid <heimir.97@gmail.com>
2020-08-26 15:43:41 +0200

    google maps enabled, dynamically render event with a map pinpointing it's location

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,1 +1,37 @@
-console.log("Hejhopp!");
+function initMap() {
+  $(document).ready(function () {
+    let map;
+    var latitude = new Array();
+    var longitude = new Array();
+    var maps = new Array();
+
+    //Get the latitude and the longtitude for from each input and push it into the arrays
+    $("input:hidden[class='lon']").each(function () {
+      var valueStr = $(this).val();
+      var valueFloat = parseFloat(valueStr);
+      longitude.push(valueFloat);
+    });
+    $("input:hidden[class='lat']").each(function () {
+      var valueStr = $(this).val();
+      var valueFloat = parseFloat(valueStr);
+      latitude.push(valueFloat);
+    });
+
+    //get all the maps and sort them in a array based on their ID
+    $(".map").each(function () {
+      mapID = $(this).attr("id");
+      maps.push(mapID);
+    });
+
+    for (i = 0; i < maps.length; i++) {
+      console.log(maps[i]);
+      position = { lat: latitude[i], lng: longitude[i] };
+
+      map = new google.maps.Map(document.getElementById(maps[i]), {
+        center: position,
+        zoom: 8,
+      });
+      var marker = new google.maps.Marker({ position: position, map: map });
+    }
+  });
+}
