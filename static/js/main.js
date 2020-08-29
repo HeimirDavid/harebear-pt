4ad732f7ba8d2879be5ed83c310c40ea14e55218
HeimirDavid <heimir.97@gmail.com>
2020-08-29 13:56:47 +0200

    Added intersection observers to change background of the navbar when scrolling down the page

--- static/js/main.js
+++ static/js/main.js
@@ -8,4 +8,27 @@ $(document).ready(function () {
     $(".navbar-nav").toggleClass("add-margin");
     $(".navbar").toggleClass("open");
   });
+
+  const navbar = document.querySelector("#mainNavbar");
+  const sectionOne = document.querySelector(".fullscreen-video-wrap");
+
+  const sectionOneOptions = {
+    rootMargin: "-130px 0px 0px 0px",
+  };
+
+  const sectionOneObserver = new IntersectionObserver(function (
+    entries,
+    sectionOneObserver
+  ) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        navbar.classList.add("nav-scrolled");
+      } else {
+        navbar.classList.remove("nav-scrolled");
+      }
+    });
+  },
+  sectionOneOptions);
+
+  sectionOneObserver.observe(sectionOne);
 });
