85194cee1eb1efdb68d3fec220d3546b4b5e798d
HeimirDavid <heimir.97@gmail.com>
2020-09-06 23:33:33 +0200

    Remade styling on the contact page, and styled card on the group training page

--- static/js/maps.js
+++ static/js/maps.js
@@ -9,27 +9,6 @@ function initMap() {
     var styledMapType = new google.maps.StyledMapType([
       {
         featureType: "all",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "on",
-          },
-        ],
-      },
-      {
-        featureType: "all",
-        elementType: "labels",
-        stylers: [
-          {
-            visibility: "off",
-          },
-          {
-            saturation: "-100",
-          },
-        ],
-      },
-      {
-        featureType: "all",
         elementType: "labels.text.fill",
         stylers: [
           {
@@ -41,9 +20,6 @@ function initMap() {
           {
             lightness: 40,
           },
-          {
-            visibility: "off",
-          },
         ],
       },
       {
@@ -51,7 +27,7 @@ function initMap() {
         elementType: "labels.text.stroke",
         stylers: [
           {
-            visibility: "off",
+            visibility: "on",
           },
           {
             color: "#000000",
@@ -110,77 +86,14 @@ function initMap() {
         ],
       },
       {
-        featureType: "landscape",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#4d6059",
-          },
-        ],
-      },
-      {
-        featureType: "landscape",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#4d6059",
-          },
-        ],
-      },
-      {
-        featureType: "landscape.natural",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#4d6059",
-          },
-        ],
-      },
-      {
         featureType: "poi",
         elementType: "geometry",
         stylers: [
+          {
+            color: "#000000",
+          },
           {
             lightness: 21,
-          },
-        ],
-      },
-      {
-        featureType: "poi",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#4d6059",
-          },
-        ],
-      },
-      {
-        featureType: "poi",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#4d6059",
-          },
-        ],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [
-          {
-            visibility: "on",
-          },
-          {
-            color: "#7f8d89",
-          },
-        ],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#7f8d89",
           },
         ],
       },
@@ -189,7 +102,7 @@ function initMap() {
         elementType: "geometry.fill",
         stylers: [
           {
-            color: "#7f8d89",
+            color: "#000000",
           },
           {
             lightness: 17,
@@ -201,7 +114,7 @@ function initMap() {
         elementType: "geometry.stroke",
         stylers: [
           {
-            color: "#7f8d89",
+            color: "#000000",
           },
           {
             lightness: 29,
@@ -224,24 +137,6 @@ function initMap() {
         ],
       },
       {
-        featureType: "road.arterial",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#7f8d89",
-          },
-        ],
-      },
-      {
-        featureType: "road.arterial",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#7f8d89",
-          },
-        ],
-      },
-      {
         featureType: "road.local",
         elementType: "geometry",
         stylers: [
@@ -250,24 +145,6 @@ function initMap() {
           },
           {
             lightness: 16,
-          },
-        ],
-      },
-      {
-        featureType: "road.local",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#7f8d89",
-          },
-        ],
-      },
-      {
-        featureType: "road.local",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#7f8d89",
           },
         ],
       },
@@ -285,88 +162,13 @@ function initMap() {
       },
       {
         featureType: "water",
-        elementType: "all",
-        stylers: [
-          {
-            color: "#2b3638",
-          },
-          {
-            visibility: "on",
-          },
-        ],
-      },
-      {
-        featureType: "water",
         elementType: "geometry",
         stylers: [
           {
-            color: "#2b3638",
+            color: "#000000",
           },
           {
             lightness: 17,
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#24282b",
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#24282b",
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "labels",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.icon",
-        stylers: [
-          {
-            visibility: "off",
           },
         ],
       },
