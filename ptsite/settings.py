09ca4427bcb5ee80d74b2fed78f4d95589763728
HeimirDavid <heimir.97@gmail.com>
2020-09-10 15:42:15 +0200

    Resized columns for text in abou.html and one_on_one.html

--- ptsite/settings.py
+++ ptsite/settings.py
@@ -33,7 +33,7 @@ GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.harebearpt.com',
                 'hare-bear-pt.herokuapp.com',
